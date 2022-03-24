197d7af2bc10f7f8bb41b3a25e15d7298a00a830
Bablu882 <babluartist057@gmail.com>
2022-03-24 18:22:52 +0530

    simplecrud/crud/views.py

--- testproject/project1/simplecrud/crud/views.py
+++ testproject/project1/simplecrud/crud/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def my_view(request):
     resp=render(request,'home.html')
+    return  resp  
+
+def test1_view(request):
+    resp=render(request,'index.html')
+    return resp
+
+
+def test2_view(request):
+    resp=render(request,'home2.html')
     return resp
